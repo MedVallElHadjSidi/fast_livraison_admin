7f4d32dcb749f6fe21d866f7a5a878bdd5493a44
MedVall <elhadjsidimv@gmail.com>
2026-08-17 14:53:45 +0000

    api key google map

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBiEXEJFNBqZVyYcEx9jNMcxsSB5B5Epzs',
+    apiKey: 'AIzaSyALKHw2yxrsMaMwquheHpvaqWlj6G5zWX4',
     authDomain: 'fasttossel-db55d.firebaseapp.com',
     projectId: 'fasttossel-db55d',
     storageBucket: 'fasttossel-db55d.firebasestorage.app',
     messagingSenderId: '1028398623935',
     appId: '1:1028398623935:web:0549a242341bd550012a0a',
   },
-  mapsApiKey: 'AIzaSyBiEXEJFNBqZVyYcEx9jNMcxsSB5B5Epzs',
+  mapsApiKey: 'AIzaSyALKHw2yxrsMaMwquheHpvaqWlj6G5zWX4',
 };
